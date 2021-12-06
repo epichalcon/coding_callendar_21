7b9f08479d863ca75e1b311a488d91462a956c24
epichalcon <nicolashirra115@gmail.com>
2021-12-06 11:35:14 +0100

    Day6 second star

--- day6.py
+++ day6.py
@@ -18,4 +18,33 @@ def problem1(data,iterations):# muy ineficiente
         i += 1
     return len(data)
 
-print(problem1(data, 256))
+def problem2(data, iterations): # complegidad n*m donde n es el numero de iteraciones y m es el tamaño del mapa
+    mapa = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0
+    }
+    suma = 0
+    for fish in data:
+        mapa[fish] += 1
+        suma += 1
+    i = 0
+    while i < iterations:
+        j = 1
+        nuevos = mapa.get(0)
+        while j < 9:
+            mapa[j-1] = mapa.get(j) 
+            j += 1
+        mapa[6] += nuevos
+        mapa[8] = nuevos
+        suma += nuevos
+        i += 1
+    return suma
+
+print(problem2(data, 256))
