a2d4e75280eab5719f02f3f6762e1ba0d3cf8121
epichalcon <nicolashirra115@gmail.com>
2021-12-06 11:48:06 +0100

    Day6 correccion (uso de lista)

--- day6.py
+++ day6.py
@@ -18,31 +18,21 @@ def problem1(data,iterations):# muy ineficiente
         i += 1
     return len(data)
 
-def problem2(data, iterations): # complegidad O(n)
-    mapa = {
-        0: 0,
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-        7: 0,
-        8: 0
-    }
+def problem2(data, iterations): # complegidad o(n)
+    lista = [0 for i in range(9)]
     suma = 0
     for fish in data:
-        mapa[fish] += 1
+        lista[fish] += 1
         suma += 1
     i = 0
     while i < iterations:
         j = 1
-        nuevos = mapa.get(0)
+        nuevos = lista[0]
         while j < 9:
-            mapa[j-1] = mapa.get(j) 
+            lista[j-1] = lista[j] 
             j += 1
-        mapa[6] += nuevos
-        mapa[8] = nuevos
+        lista[6] += nuevos
+        lista[8] = nuevos
         suma += nuevos
         i += 1
     return suma
