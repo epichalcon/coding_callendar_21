34a9792b4d314c3fded105a29eaed0e2840cdde2
epichalcon <nicolashirra115@gmail.com>
2021-12-08 12:23:29 +0100

    Day8 second Star

--- day8.py
+++ day8.py
@@ -1,10 +1,12 @@
+#https://adventofcode.com/2021/day/8
 
+from operator import le
 from numpy.core.fromnumeric import mean
 from getdata.getdata import GetData as gd
 from constants.constants import  DIRECTORY
 import numpy as np
 import math
-data = gd.getdata(f"{DIRECTORY}prueva.txt")
+data = gd.getdata(f"{DIRECTORY}day8.txt")
 data = gd.separarPorLineas(data)
 
 def problem1(data):
@@ -17,4 +19,64 @@ def problem1(data):
                 suma += 1
     return suma
 
-print(problem1(data))
+
+def toBinary(number):
+    binary = ['0' for i in range(7)]
+    for letter in number:
+        binary[ord(letter) - 97] = '1'
+    return ''.join(binary)
+
+def rewire(numbers):
+    len_map = {}
+    for number in numbers:
+        if len_map.get(len(number), None) == None:
+            len_map[len(number)] = [toBinary(number)]
+        else:
+            len_map[len(number)].append(toBinary(number))
+    
+    code = {}
+    for number in len_map[6]:
+        if int(number,2) | int(len_map[4][0],2) == int(number,2):
+            code[number] = '9'
+        elif int(number,2) | int(len_map[3][0],2) == int(number,2):
+            code[number] = '0'
+        else:
+            top_rigth_led = int(number,2)^int(len_map[7][0],2)
+            code[number] = '6'
+
+
+    for number in len_map[5]:
+        if int(number,2) | int(len_map[2][0],2) == int(number,2):
+            code[number] = '3'
+        elif int(number,2) | top_rigth_led == int(number,2):
+            code[number] = '2'
+        else:
+            code[number] = '5'
+
+    return code
+
+def decode(code, numbers):
+    res = ''
+    for number in numbers:
+        if len(number) == 2:
+            res += '1'
+        elif len(number) == 3:
+            res += '7'
+        elif len(number) == 4:
+            res += '4'
+        elif len(number) == 7:
+            res += '8'
+        else:
+            res += code[toBinary(number)]
+        
+    return int(res, 10)
+
+def problem2(data):
+    suma = 0
+    for linea in data:
+        splited = linea.split(' | ')
+        coded = rewire(splited[0].split(' '))
+        suma += decode(coded, splited[1].split(' '))
+    return suma
+
+print(problem2(data))
