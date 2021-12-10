cd59bb3d2f54a7b17dd9baeca2b6485bbcff00dc
epichalcon <nicolashirra115@gmail.com>
2021-12-10 12:40:59 +0100

    Day10 first Star 2.0

--- day10.py
+++ day10.py
@@ -5,27 +5,48 @@ from constants.constants import  DIRECTORY
 data = gd.getdata(f"{DIRECTORY}prueva.txt")
 data = gd.separarPorLineas(data)
 
-def problem1(data) -> int:
+class CloserNotExpected(Exception):
+    closer = ''
+    def __init__(self, closer) -> None:
+        self.closer = closer
+
+
+
+def syntaxChecker(line) -> list:
     openers = ('(','[','{','<')
     closers = (')',']','}','>')
+    stack = []
+    for char in line:
+        try:
+            opener = openers.index(char)
+            stack.append(opener)
+        except ValueError:
+            closer = closers.index(char)
+            lastOpener = stack.pop()
+            if closer != lastOpener:
+                raise CloserNotExpected(closer)
+    return stack
+
+
+
+
+def problem1(data) -> int:
+    res = 0
     points = {
         0: 3,
         1:57,
         2:1197,
         3:25137
     }
-    res = 0
     for line in data:
-        stack = []
-        for char in line:
-            try:
-                opener = openers.index(char)
-                stack.append(opener)
-            except ValueError:
-                closer = closers.index(char)
-                lastOpener = stack.pop()
-                if closer != lastOpener:
-                    res += points[closer]
+        try:
+            syntaxChecker(line)
+        except CloserNotExpected as exc:
+            res += points[exc.closer]
+
     return res
+
+
+
 
 print(problem1(data))
